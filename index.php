aac08814ee1cc8817fd7206b6bf2cdea8a902eb6
Gabriel <gamoreirarolim@gmail.com>
2024-04-18 08:46:37 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estacionamento</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="container p-16 mx-auto h-16 flex" >
+    <h1 class="text-xl">Estacionamento</h1>
+    <div class="container p-16 mx-auto h-16 flex " >
+        <div class="w-full max-w-xs">
+            Entrada de Veículos
+        </div>
+        <div class="w-full max-w-xs">
+            Saída de Veículos
+        </div>
+    </div>
+</div>
+</body>
+</html>
